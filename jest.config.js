36b8c220e27b94256b1f6270563e3ccbdfcf95a3
celli33 <limon.martinez.misael@gmail.com>
2021-07-06 18:50:11 -0500

    chore: fix eslint and coverage

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['jest-extended'],
+  collectCoverageFrom: ['src/**'],
 };
