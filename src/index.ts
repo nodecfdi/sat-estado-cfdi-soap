b460de4671a05c6ce7d810c9964d791003881239
celli33 <limon.martinez.misael@gmail.com>
2021-07-06 18:51:57 -0500

    chore: prepare for publish

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 /** Hello world */
+import { SoapClientFactory } from './SoapClientFactory';
+import { SoapConsumerClient } from './SoapConsumerClient';
+
+export { SoapClientFactory, SoapConsumerClient };
+
+Object.assign(module.exports, SoapConsumerClient);
+Object.assign(module.exports, SoapClientFactory);
